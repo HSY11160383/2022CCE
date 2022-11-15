2811cdba49bd037b466574006067a599bddf82bc
HSY11160383 <11160383@me.mcu.edu.tw>
2022-11-15 12:00:44 +0800

    week11

--- week02-1.c
+++
@@ -1,9 +0,0 @@
-///file-new-empty空白檔案
-///file-save file As 存成 week02-1.ccp
-#include <stdio.h>
-int main()
-{
-    printf("Hello World\n");
-    printf("H\Hello World\n");
-    printf("\tHello World\n");
-}
